b5d0eebeae93f284c3a83e638521db2fe0009cb4
Dmitry Iv. <df.creative@gmail.com>
2025-03-08 11:49:13 -0500

    Reorganize normalize

--- src/normalize.js
+++ src/normalize.js
@@ -8,32 +8,89 @@ import { SECTION } from './const.js'
 export default function normalize(nodes) {
   if (typeof nodes === 'string') nodes = parse(nodes)
 
-  let from = 0, idx = 0 // reusable within-node pointer
-
   // (module ...) abbr https://webassembly.github.io/spec/core/text/modules.html#id10
-  if (nodes[0] === 'module') {
-    from = nodes[1]?.[0] === '$' ? 2 : 1
-  }
+  if (nodes[0] === 'module') nodes = nodes.slice(nodes[1]?.[0] === '$' ? 2 : 1)
   // (op ...) single node
   else if (typeof nodes[0] === 'string') nodes = [nodes]
 
-  if (nodes[from] === 'binary' || nodes[from] === 'quote') return nodes.slice(from) // FIXME: make more clever, maybe join data definitions?
+  // (module binary "..."), (module quote "...")
+  if (nodes[0] === 'binary' || nodes[0] === 'quote') return nodes
 
-  const sections = [], _types = []
-  for (let kind in SECTION) sections[SECTION[kind]] = []
+  const sections = [[], [], [], [], [], [], [], [], [], [], [], []]
 
-  // TODO: dealias section names
+  // consume typeuse nodes, return type index/params, or null idx if no type
+  // https://webassembly.github.io/spec/core/text/modules.html#type-uses
+  const typeuse = (node, names) => {
+    let id, param, result
 
-  // process node (put into corresponding section)
-  const normalizeNode = (node) => {
-    idx = 0 // within-node indx
-    let kind = node[0]
+    // explicit type (type 0|$name)
+    if (node[idx]?.[0] === 'type') {
+      [, id] = node[idx];
+    }
+
+    // implicit type (param i32 i32)(result i32)
+    [param, result] = paramres(node, names)
+
+    // check type consistency
+    // if ((param.length || result.length) && id in sections[SECTION.type])
+    //   if (sections[SECTION.type][deref(id, sections[SECTION.type])][1].join('>') !== param + '>' + result) err(`Type ${id} mismatch`)
+
+    return [id, param, result]
+  }
+
+  // consume (param t+)* (result t+)* sequence
+  const paramres = (node, names = true) => {
+    // collect param (param i32 i64) (param $x? i32)
+    let param = fieldseq(node, 'param', names)
+
+    // collect result eg. (result f64 f32)(result i32)
+    let result = fieldseq(node, 'result')
+
+    if (node[0]?.[0] === 'param') err(`Unexpected param`)
+
+    return [param, result]
+  }
+
+  // collect sequence of field, eg. (param a) (param b c), (field a) (field b c) or (result a b) (result c)
+  // optionally allow or not names
+  const fieldseq = (node, field, names = false) => {
+    let seq = []
+    // collect field eg. (field f64 f32)(field i32)
+    while (node[idx]?.[0] === field) {
+      let [, ...args] = node[idx++]
+      // (field $x type)
+      if (args[0]?.[0] === '$') {
+        let [name, type] = args
+        if (names) name in seq ? err(`Duplicate ${field} ${name}`) : seq[name] = seq.length
+        else err(`Unexpected ${field} name ${name}`)
+        seq.push(type)
+      }
+      // (field type*)
+      else seq.push(...args)
+    }
+    return seq
+  }
+
+  // deref id node to numeric idx
+  // const deref = (nm, list, n) => (n = nm[0] === '$' ? list[nm] : +nm, n in list ? n : err(`Unknown ${list.name} ${nm}`))
+
+  let idx = 0 // reusable within-node pointer
+
+  // collect & normalize types
+  nodes.filter(node => {
+    let [kind] = node
 
     // (rec (type $a (sub final? $sup* (func ...))...) (type $b ...)) -> save subtypes
-    if (kind === 'rec') {
-      sections[SECTION.type].push(node);
-      return
-    }
+    if (kind === 'rec' || kind === 'type') sections[SECTION.type].push(node)
+    else if (kind === 'export' || kind === 'start' || kind === 'elem' || kind === 'data') sections[SECTION[kind]].push(node)
+    else return true
+  })
+
+  // reorder nodes by sections, deabbr
+  .forEach((node) => {
+    // TODO: dealias section names
+    idx = 0 // within-node indx
+    let kind = node[0], imported = 0
 
     // import abbr
     // (import m n (table|memory|global|func id? type)) -> (table|memory|global|func id? (import m n) type)
@@ -46,118 +103,58 @@ export default function normalize(nodes) {
 
     let items = sections[SECTION[kind]];
 
-    if (kind === 'export' || kind === 'start' || kind === 'elem' || kind === 'data') {
-      items.push(node)
-    }
-
     // (kind === 'type' || kind === 'table' || kind === 'memory' || kind === 'global' || kind === 'func')
-    else {
-      // index, alias
-      idx = 1 // within-node idx
-      let name = (node[idx]?.[0] === '$') && node[idx++]
 
-      // export abbr
-      // (table|memory|global|func id? (export n)* ...) -> (table|memory|global|func id ...) (export n (table|memory|global|func id))
-      while (node[idx]?.[0] === 'export') sections[SECTION.export].push([...node[idx++], [kind, items.length]])
+    // index, alias
+    idx = 1 // within-node idx
+    let name = (node[idx]?.[0] === '$') && node[idx++]
 
-      // table abbr
-      // (table id? reftype (elem ...n)) -> (table id? n n reftype) (elem (table id) (i32.const 0) reftype ...)
-      if (kind === 'table' && node[idx + 1]?.[0] === 'elem') {
-        let reftype = node[idx], [, ...els] = node[idx + 1]
-        idx = 0, node = [els.length, els.length, reftype]
-        sections[SECTION.elem].push(['elem', ['table', name || items.length], ['i32.const', '0'], reftype, ...els])
-      }
+    // export abbr
+    // (table|memory|global|func id? (export n)* ...) -> (table|memory|global|func id ...) (export n (table|memory|global|func id))
+    while (node[idx]?.[0] === 'export') sections[SECTION.export].push([...node[idx++], [kind, items.length]])
 
-      // data abbr
-      // (memory id? (data str)) -> (memory id? n n) (data (memory id) (i32.const 0) str)
-      else if (kind === 'memory' && node[idx]?.[0] === 'data') {
-        let [, ...data] = node[idx],
-          m = ('' + Math.ceil(data.reduce((s, str) => (s + str.length - 2), 0) / 65536)) // FIXME: figure out precise data size
-        sections[SECTION.data].push(['data', ['memory', items.length], ['i32.const', 0], ...data])
-        idx = 0, node = [m, m]
-      }
+    // skip import
+    if (node[idx]?.[0] === 'import') idx++, imported = 1;
 
-      // resolve type
-      // // (func id? export* import? typeuse local* instr*), typeuse = (type idx?)? param* result*
-      // else if (kind === 'func') {
-      //   let [idx, param, result] = typeuse(node);
-      //   idx ?? (_types[idx = '$' + param + '>' + result] = [param, result]); // infer type
-
-
-      //   // we save idx because type can be defined after
-      //   !imported && nodes.push(['code', [idx, param, result], ...plain(node, ctx)]) // pass param since they may have names
-
-
-      //   node.unshift(['type', idx])
-      // }
-
-      items.push([kind, name || `(;${items.length};)`, ...node.slice(idx)])
-    }
-  }
-
-  // consume typeuse nodes, return type index/params, or null idx if no type
-  // https://webassembly.github.io/spec/core/text/modules.html#type-uses
-  const typeuse = (node, names) => {
-    let id, param, result
-
-    // explicit type (type 0|$name)
-    if (node[idx]?.[0] === 'type') {
-      [, id] = node[idx];
-      [param, result] = paramres(node, names);
-
-      // check type consistency (excludes forward refs)
-      // if ((param.length || result.length) && id in sections[SECTION.type])
-      //   if (sections[SECTION.type][id(id, sections.type)][1].join('>') !== param + '>' + result) err(`Type ${id} mismatch`)
-
-      return [id]
+    // table abbr
+    // (table id? reftype (elem ...n)) -> (table id? n n reftype) (elem (table id) (i32.const 0) reftype ...)
+    if (kind === 'table' && node[idx + 1]?.[0] === 'elem') {
+      let reftype = node[idx], [, ...els] = node[idx + 1]
+      idx = 0, node = [els.length, els.length, reftype]
+      sections[SECTION.elem].push(['elem', ['table', name || items.length], ['i32.const', '0'], reftype, ...els])
     }
 
-    // implicit type (param i32 i32)(result i32)
-    [param, result] = paramres(node, names)
+    // data abbr
+    // (memory id? (data str)) -> (memory id? n n) (data (memory id) (i32.const 0) str)
+    else if (kind === 'memory' && node[idx]?.[0] === 'data') {
+      let [, ...data] = node[idx],
+        m = ('' + Math.ceil(data.reduce((s, str) => (s + str.length - 2), 0) / 65536)) // FIXME: figure out precise data size
+      sections[SECTION.data].push(['data', ['memory', items.length], ['i32.const', 0], ...data])
+      idx = 0, node = [m, m]
+    }
 
-    return [, param, result]
-  }
+    // resolve type
+    // (func id? export* import? typeuse local* instr*), typeuse = (type idx?)? param* result*
+    // else if (kind === 'func') {
+    //   let [typeid, param, result] = typeuse(node);
+    //   typeid ?? (_types[typeid = '$' + param + '>' + result] ||= [param, result]); // infer type
 
-  // consume (param t+)* (result t+)* sequence
-  const paramres = (nodes, names = true) => {
-    // let param = [], result = []
+    //   // consume locals
 
-    // collect param (param i32 i64) (param $x? i32)
-    let param = fieldseq(nodes, 'param', names)
+    //   let code = plain(node)
 
-    // collect result eg. (result f64 f32)(result i32)
-    let result = fieldseq(nodes, 'result')
+    //   // // we save typeid because type can be defined after
+    //   // nodes.push(['code', [typeid, param, result], ...plain(node, ctx)]) // pass param since they may have names
 
-    if (nodes[0]?.[0] === 'param') err(`Unexpected param`)
+    //   node = [['type', typeid], ...code]
+    // }
 
-    return [param, result]
-  }
-
-  // reorder nodes by sections, deabbr
-  for (let i = from; i < nodes.length; i++) normalizeNode(nodes[i])
+    items.push([kind, name || `(;${items.length};)`, ...node.slice(idx - imported)])
+  })
 
   // TODO: push implicit types
 
   // TODO: plainify func, resolve types
 
   return sections.flat()
-}
-
-
-// collect sequence of field, eg. (param a) (param b c), (field a) (field b c) or (result a b) (result c)
-// optionally allow or not names
-const fieldseq = (nodes, field, names = false) => {
-  let seq = []
-  // collect field eg. (field f64 f32)(field i32)
-  while (nodes[0]?.[0] === field) {
-    let [, ...args] = nodes.shift()
-    let name = args[0]?.[0] === '$' && args.shift()
-    // expose name refs, if allowed
-    if (name) {
-      if (names) name in seq ? err(`Duplicate ${field} ${name}`) : seq[name] = seq.length
-      else err(`Unexpected ${field} name ${name}`)
-    }
-    seq.push(...args)
-  }
-  return seq
 }
