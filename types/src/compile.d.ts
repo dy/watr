a30bf915658709619cd37d1b36229bf2dd9e0e7a
Dmitry Iv. <df.creative@gmail.com>
2026-01-21 12:45:47 -0500

    Cleanup repo

--- types/src/compile.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Converts a WebAssembly Text Format (WAT) tree to a WebAssembly binary format (WASM).
- *
- * @param {string|Array} nodes - The WAT tree or string to be compiled to WASM binary.
- * @returns {Uint8Array} The compiled WASM binary data.
- */
-export default function compile(nodes: string | any[]): Uint8Array;
-//# sourceMappingURL=compile.d.ts.map
--- types/src/parse.d.ts
+++
@@ -1,3 +0,0 @@
-declare function _default(str: string): any[];
-export default _default;
-//# sourceMappingURL=parse.d.ts.map
